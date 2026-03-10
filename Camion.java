3b68d0752d2d79fecb75879fa4ba77b74e1a4e75
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-03-10 00:36:30 -0600

    constructores recibe

--- Camion.java
+++ Camion.java
@@ -3,5 +3,12 @@ public class Camion extends Vehiculo {
     protected int capacidadCarga;
     protected int numeroEjes;
 
-    
+    public Camion(String marca, String modelo, int anio, double precio, int capacidadCarga, int numeroEjes) {
+
+        super(marca, modelo, anio, precio);
+        this.capacidadCarga = capacidadCarga;
+        this.numeroEjes = numeroEjes;
+    }
+
+   
 }
