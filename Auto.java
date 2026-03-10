7f604ba286c549152a2078a4252c1c6284781ca4
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-03-10 00:19:06 -0600

    atributos en la clase Veiculo

---
+++ Auto.java
@@ -0,0 +1,21 @@
+public class Auto extends Vehiculo {
+
+    protected int numeroPuertas;
+    protected String tipoCombustible;
+
+    public Auto(String marca, String modelo, int anio, double precio,
+                int numeroPuertas, String tipoCombustible) {
+
+        super(marca, modelo, anio, precio);
+        this.numeroPuertas = numeroPuertas;
+        this.tipoCombustible = tipoCombustible;
+    }
+
+    public void tocarClaxon() {
+        System.out.println("El auto " + getMarca() + " toca el claxon");
+    }
+
+    public void mostrarPuertas() {
+        System.out.println("Número de puertas: " + numeroPuertas);
+    }
+}
