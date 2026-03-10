5218b5ea5853aa6236e0b646d6d28d761754b359
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-03-10 00:35:52 -0600

    atributos camion

--- Camion.java
+++ Camion.java
@@ -3,19 +3,5 @@ public class Camion extends Vehiculo {
     protected int capacidadCarga;
     protected int numeroEjes;
 
-    public Camion(String marca, String modelo, int anio, double precio,
-                  int capacidadCarga, int numeroEjes) {
-
-        super(marca, modelo, anio, precio);
-        this.capacidadCarga = capacidadCarga;
-        this.numeroEjes = numeroEjes;
-    }
-
-    public void cargarMercancia() {
-        System.out.println("El camión " + getMarca() + " está cargando mercancía");
-    }
-
-    public void mostrarCapacidad() {
-        System.out.println("Capacidad de carga: " + capacidadCarga);
-    }
+    
 }
