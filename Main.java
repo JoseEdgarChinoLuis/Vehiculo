260f0bf65d357c46a6a6c5449e199323397d65e8
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-03-10 00:39:40 -0600

    creamos las opciones

--- Main.java
+++ Main.java
@@ -12,47 +12,5 @@ public class Main {
 
         opcion = pd.pedirEntero("Seleccione una opción");
 
-        if (opcion == 1) {
-
-            String marca = pd.pedirCadena("Marca");
-            String modelo = pd.pedirCadena("Modelo");
-            int ano = pd.pedirEntero("Año");
-            double precio = pd.pedirDecimal("Precio");
-            int numPuertas = pd.pedirEntero("Número de puertas");
-            String tipoCombustible = pd.pedirCadena("Tipo de combustible");
-
-            Auto auto1 = new Auto(marca, modelo, ano, precio, numPuertas, tipoCombustible);
-
-            auto1.mostrarInfo();
-            auto1.tocarClaxon();
-            auto1.mostrarPuertas();
-            auto1.encender();
-        }
-
-        else if (opcion == 2) {
-
-            String marca = pd.pedirCadena("Marca");
-            String modelo = pd.pedirCadena("Modelo");
-            int ano = pd.pedirEntero("Año");
-            double precio = pd.pedirDecimal("Precio");
-            int capacidadCarga = pd.pedirEntero("Capacidad de carga");
-            int numeroEjes = pd.pedirEntero("Número de ejes");
-
-            Camion camion1 = new Camion(marca, modelo, ano, precio, capacidadCarga, numeroEjes);
-
-            camion1.mostrarInfo();
-            camion1.cargarMercancia();
-            camion1.mostrarCapacidad();
-            camion1.encender();
-        }
-
-        else if (opcion == 3) {
-
-            System.out.println("Total de vehículos creados: " + Vehiculo.totalVehiculos);
-        }
-
-        else {
-            System.out.println("Opción no válida");
-        }
-    }
+        
 }
