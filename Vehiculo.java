3430cb38f6916892a55d1e8368f56b671eaa50c0
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-03-10 00:28:05 -0600

    se agrego los getters y stters

--- Vehiculo.java
+++ Vehiculo.java
@@ -34,5 +34,37 @@ public class Vehiculo {
         precio = precio * 0.9;
     }
 
-    
+    // GETTERS Y SETTERS
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
