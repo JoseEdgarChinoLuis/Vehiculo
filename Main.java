9a80d71d4063c3b8ac069bb1b5c7d09f4333aede
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-03-10 00:40:25 -0600

    creamos el primer auto

--- Main.java
+++ Main.java
@@ -12,5 +12,22 @@ public class Main {
 
         opcion = pd.pedirEntero("Seleccione una opción");
 
-        
+        if (opcion == 1) {
+
+            String marca = pd.pedirCadena("Marca");
+            String modelo = pd.pedirCadena("Modelo");
+            int ano = pd.pedirEntero("Año");
+            double precio = pd.pedirDecimal("Precio");
+            int numPuertas = pd.pedirEntero("Número de puertas");
+            String tipoCombustible = pd.pedirCadena("Tipo de combustible");
+
+            Auto auto1 = new Auto(marca, modelo, ano, precio, numPuertas, tipoCombustible);
+
+            auto1.mostrarInfo();
+            auto1.tocarClaxon();
+            auto1.mostrarPuertas();
+            auto1.encender();
+        }
+
+       
 }
