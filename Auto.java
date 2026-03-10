0a0fbef0204fe629b46f6a5e8a14a4d0b839a932
JoseEdgarChinoLuis <20253rd009@utez.edu.mx>
2026-03-10 00:33:10 -0600

    costructor

--- Auto.java
+++ Auto.java
@@ -3,5 +3,12 @@ public class Auto extends Vehiculo {
     protected int numeroPuertas;
     protected String tipoCombustible;
 
+    public Auto(String marca, String modelo, int anio, double precio, int numeroPuertas, String tipoCombustible) {
+
+        super(marca, modelo, anio, precio);
+        this.numeroPuertas = numeroPuertas;
+        this.tipoCombustible = tipoCombustible;
+    }
+
     
 }
